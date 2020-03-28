dff8d640c765c986415e85744fbe50cc4622bf42
Greg Walker <mgwalker@gmail.com>
2020-03-27 22:05:55 -0500

    today in history

---
+++ .github/workflows/today.py
@@ -0,0 +1,61 @@
+import chevron
+import json
+from shutil import copyfile
+from datetime import date, timedelta
+
+template = open("today/day.mustache", "r")
+
+
+def get_date_from_str(short_date):
+    return date(2001, int(short_date[:2]), int(short_date[2:]))
+
+
+def get_str_from_date(date):
+    return f"{date.month if date.month >= 10 else f'0{date.month}'}{date.day if date.day >= 10 else f'0{date.day}'}"
+
+
+with open("today/index.json", "r") as f:
+    today_meta = json.load(f)
+    f.close()
+
+    now = get_str_from_date(date.today())
+
+    for day in today_meta:
+        template.seek(0)
+        short_date = day["short_date"]
+
+        this_day = get_date_from_str(short_date)
+
+        for entry in day["entries"]:
+            copyfile(f"today/{entry['file']}", f"docs/today/{entry['file']}")
+
+        next_date = get_str_from_date(this_day + timedelta(1))
+        next_date = (
+            next_date
+            if any(d["short_date"] == next_date for d in today_meta)
+            else False
+        )
+        prev_date = get_str_from_date(this_day + timedelta(-1))
+        prev_date = (
+            prev_date
+            if any(d["short_date"] == prev_date for d in today_meta)
+            else False
+        )
+
+        template_data = {
+            **day,
+            "hero": day["entries"][0]["file"],
+            "title": f"{day['entries'][0]['year']}: {day['entries'][0]['title']}",
+            "next_date": next_date,
+            "prev_date": prev_date,
+        }
+
+        with open(f"docs/today/{short_date}.html", "w") as day_file:
+            day_file.write(chevron.render(template, template_data))
+            day_file.close()
+
+        if short_date == now:
+            with open(f"docs/today/index.html", "w") as index_file:
+                template.seek(0)
+                index_file.write(chevron.render(template, template_data))
+                index_file.close()
