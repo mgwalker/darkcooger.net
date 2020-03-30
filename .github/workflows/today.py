8d025e24ab6613d0967c0863ffb2d34554360469
Greg Walker <mgwalker@gmail.com>
2020-03-29 20:58:46 -0500

    only update today's stuff, so the latest drawings only go up on the day-of, not early

--- .github/workflows/today.py
+++ .github/workflows/today.py
@@ -18,44 +18,41 @@ with open("today/index.json", "r") as f:
     today_meta = json.load(f)
     f.close()
 
-    now = get_str_from_date(date.today())
+now = get_str_from_date(date.today())
 
-    for day in today_meta:
+day = next(d for d in today_meta if d["short_date"] == now)
+
+template.seek(0)
+short_date = day["short_date"]
+
+this_day = get_date_from_str(short_date)
+
+for entry in day["entries"]:
+    copyfile(f"today/{entry['file']}", f"docs/today/{entry['file']}")
+
+next_date = get_str_from_date(this_day + timedelta(1))
+next_date = (
+    next_date if any(d["short_date"] == next_date for d in today_meta) else False
+)
+prev_date = get_str_from_date(this_day + timedelta(-1))
+prev_date = (
+    prev_date if any(d["short_date"] == prev_date for d in today_meta) else False
+)
+
+template_data = {
+    **day,
+    "hero": day["entries"][0]["file"],
+    "title": f"{day['entries'][0]['year']}: {day['entries'][0]['title']}",
+    "next_date": next_date,
+    "prev_date": prev_date,
+}
+
+with open(f"docs/today/{short_date}.html", "w") as day_file:
+    day_file.write(chevron.render(template, template_data))
+    day_file.close()
+
+if short_date == now:
+    with open(f"docs/today/index.html", "w") as index_file:
         template.seek(0)
-        short_date = day["short_date"]
-
-        this_day = get_date_from_str(short_date)
-
-        for entry in day["entries"]:
-            copyfile(f"today/{entry['file']}", f"docs/today/{entry['file']}")
-
-        next_date = get_str_from_date(this_day + timedelta(1))
-        next_date = (
-            next_date
-            if any(d["short_date"] == next_date for d in today_meta)
-            else False
-        )
-        prev_date = get_str_from_date(this_day + timedelta(-1))
-        prev_date = (
-            prev_date
-            if any(d["short_date"] == prev_date for d in today_meta)
-            else False
-        )
-
-        template_data = {
-            **day,
-            "hero": day["entries"][0]["file"],
-            "title": f"{day['entries'][0]['year']}: {day['entries'][0]['title']}",
-            "next_date": next_date,
-            "prev_date": prev_date,
-        }
-
-        with open(f"docs/today/{short_date}.html", "w") as day_file:
-            day_file.write(chevron.render(template, template_data))
-            day_file.close()
-
-        if short_date == now:
-            with open(f"docs/today/index.html", "w") as index_file:
-                template.seek(0)
-                index_file.write(chevron.render(template, template_data))
-                index_file.close()
+        index_file.write(chevron.render(template, template_data))
+        index_file.close()
