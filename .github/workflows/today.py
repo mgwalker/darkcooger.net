a2f833b890c9c1fdb083d6afa973619fb012f7f7
Greg Walker <mgwalker@gmail.com>
2020-05-18 20:57:13 -0500

    change everything

--- .github/workflows/today.py
+++ .github/workflows/today.py
@@ -1,28 +1,37 @@
 import chevron
 import json
 import os.path
+import os
 from shutil import move
 from datetime import date, timedelta
 
+now = date.today()
 template = open("today/day.mustache", "r")
 
 
 def get_date_from_str(short_date):
-    return date(2001, int(short_date[:2]), int(short_date[2:]))
+    return date(now.year, int(short_date[:2]), int(short_date[2:]))
 
 
 def get_str_from_date(date):
     return f"{date.month if date.month >= 10 else f'0{date.month}'}{date.day if date.day >= 10 else f'0{date.day}'}"
 
 
-def write_html(with_data, index=False):
-    short_date = with_data["short_date"]
+def get_date_str(date):
+    return f"{['', 'January','February','March','April','May','June','July','August','September','October','November','December'][date.month]} {date.day}"
 
+
+def get_file_name(short_date, year):
+    return f"{short_date}_{year}.jpg"
+
+
+def write_html(short_date, comics, index=False):
     this_day = get_date_from_str(short_date)
 
-    for entry in with_data["entries"]:
-        if os.path.isfile(f"today/{entry['file']}"):
-            move(f"today/{entry['file']}", f"docs/today/images/{entry['file']}")
+    for comic in comics:
+        fn = get_file_name(short_date, comic["year"])
+        if os.path.isfile(f"today/{fn}"):
+            move(f"today/{fn}", f"docs/today/images/{fn}")
 
     next_date = get_str_from_date(this_day + timedelta(1))
     next_date = next_date if os.path.isfile(f"docs/today/{next_date}.html") else False
@@ -30,11 +39,13 @@ def write_html(with_data, index=False):
     prev_date = prev_date if os.path.isfile(f"docs/today/{prev_date}.html") else False
 
     template_data = {
-        **with_data,
-        "hero": with_data["entries"][0]["file"],
-        "title": f"{with_data['entries'][0]['year']}: {with_data['entries'][0]['title']}",
+        "entries": comics,
+        "date": get_date_str(this_day),
+        "hero": get_file_name(short_date, comics[0]["year"]),
+        "title": f"{comics[0]['year']}: {comics[0]['title']}",
         "next_date": next_date,
         "prev_date": prev_date,
+        "short_date": short_date,
     }
 
     with open(f"docs/today/{short_date}.html", "w") as day_file:
@@ -53,12 +64,12 @@ with open("today/index.json", "r") as f:
     today_meta = json.load(f)
     f.close()
 
-now = get_str_from_date(date.today())
-
-today_index = [d["short_date"] == now for d in today_meta].index(True)
-
-write_html(today_meta[today_index], True)
-if today_index >= 0:
-    write_html(today_meta[today_index - 1])
+for short_date in sorted(today_meta.keys()):
+    comics = today_meta[short_date]
+    comic_date = get_date_from_str(short_date)
+    if comic_date <= now:
+        write_html(short_date, comics)
+        if comic_date == now:
+            write_html(short_date, comics, True)
 
 template.close()
