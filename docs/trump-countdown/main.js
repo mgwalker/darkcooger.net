afbdd8ec53ce1072fde86c236176d73a1c616b09
Greg Walker <mgwalker@gmail.com>
2021-01-18 10:39:22 -0600

    prepare for the finale

--- docs/trump-countdown/main.js
+++ docs/trump-countdown/main.js
@@ -1,31 +1,48 @@
-(function() {
-  var start = new Date('2017-01-20T12:00:00-0500');
-  var end = new Date('2021-01-20T12:00:00-0500');
-  var total = timediff(start, end, 'DHm');
+(function () {
+  const start = new Date("2017-01-20T12:00:00-0500");
+  const end = new Date("2021-01-20T12:00:00-0500");
+  const total = timediff(start, end, "DHm");
 
   function getTotalDays(dhmDuration) {
-    return dhmDuration.days + (dhmDuration.hours / 24) + (dhmDuration.minutes / 1440);
+    return (
+      dhmDuration.days + dhmDuration.hours / 24 + dhmDuration.minutes / 1440
+    );
   }
 
   function getPercentDone(total, remainder) {
-    var totalDays = getTotalDays(total);
-    var remainderDays = getTotalDays(remainder);
-    return Math.round((1 - (remainderDays / totalDays)) * 10000) / 100;
+    const totalDays = getTotalDays(total);
+    const remainderDays = getTotalDays(remainder);
+    return Math.round((1 - remainderDays / totalDays) * 10000) / 100;
   }
 
   function go() {
-      var remainder = timediff(new Date(), end, 'DHmS');
-      var percentDone = getPercentDone(total, remainder);
+    $(".table .cell").show();
 
-      $('div.progress-bar').css('width', percentDone + '%');
-      $('#remainder-percent').text(percentDone);
-      $('#remainder-days').text(remainder.days);
-      $('#remainder-hours').text(remainder.hours + 1);
-      $('#remainder-minutes').text(remainder.minutes);
-      $('#remainder-seconds').text(remainder.seconds);
+    if (moment().isAfter(end)) {
+      $("header").hide();
+      $(".table .cell").text(
+        "The twice-impeached seditious motherfucker is out!"
+      );
+      return;
+    }
+
+    var remainder = timediff(new Date(), end, "DHmS");
+    var percentDone = getPercentDone(total, remainder);
+
+    $("header").show();
+    $("div.progress-bar").css("width", percentDone + "%");
+    $("#remainder-percent").text(percentDone);
+    $("#remainder-days").text(remainder.days);
+    $("#remainder-days-s")[remainder.days === 1 ? "hide" : "show"]();
+    $("#remainder-hours").text(remainder.hours + 1);
+    $("#remainder-hours-s")[remainder.hours === 0 ? "hide" : "show"]();
+    $("#remainder-minutes").text(remainder.minutes);
+    $("#remainder-minutes-s")[remainder.minutes === 1 ? "hide" : "show"]();
+    $("#remainder-seconds").text(remainder.seconds);
+    $("#remainder-seconds-s")[remainder.seconds === 1 ? "hide" : "show"]();
   }
-  
-  $(document).ready(function() {
+
+  $(document).ready(function () {
     go();
     setInterval(go, 1000);
   });
