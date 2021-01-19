4fb1d277b6fc0ae3397db0cecf8844f1b4f87d62
Greg Walker <mgwalker@gmail.com>
2021-01-18 23:34:20 -0600

    getting closer

--- docs/trump-countdown/main.js
+++ docs/trump-countdown/main.js
@@ -1,19 +1,5 @@
 (function () {
-  const start = new Date("2017-01-20T12:00:00-0500");
   const end = new Date("2021-01-20T12:00:00-0500");
-  const total = timediff(start, end, "DHm");
-
-  function getTotalDays(dhmDuration) {
-    return (
-      dhmDuration.days + dhmDuration.hours / 24 + dhmDuration.minutes / 1440
-    );
-  }
-
-  function getPercentDone(total, remainder) {
-    const totalDays = getTotalDays(total);
-    const remainderDays = getTotalDays(remainder);
-    return Math.round((1 - remainderDays / totalDays) * 10000) / 100;
-  }
 
   function go() {
     $(".table .cell").show();
@@ -26,16 +12,14 @@
       return;
     }
 
-    var remainder = timediff(new Date(), end, "DHmS");
-    var percentDone = getPercentDone(total, remainder);
+    var remainder = timediff(new Date(), end, "HmS");
 
-    $("header").show();
-    $("div.progress-bar").css("width", percentDone + "%");
-    $("#remainder-percent").text(percentDone);
-    $("#remainder-days").text(remainder.days);
-    $("#remainder-days-s")[remainder.days === 1 ? "hide" : "show"]();
-    $("#remainder-hours").text(remainder.hours + 1);
-    $("#remainder-hours-s")[remainder.hours === 0 ? "hide" : "show"]();
+    if (remainder.hours < 1) {
+      $("#hours").hide();
+    }
+
+    $("#remainder-hours").text(remainder.hours);
+    $("#remainder-hours-s")[remainder.hours === 1 ? "hide" : "show"]();
     $("#remainder-minutes").text(remainder.minutes);
     $("#remainder-minutes-s")[remainder.minutes === 1 ? "hide" : "show"]();
     $("#remainder-seconds").text(remainder.seconds);
